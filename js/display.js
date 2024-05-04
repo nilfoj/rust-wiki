0a689456871dec53ddf0559551c27263d5dd542c
nilfoj <nilfoj@hotmail.com>
2024-05-03 23:23:36 -0300

    Att.
    03.05.24

---
+++ js/display.js
@@ -0,0 +1,25 @@
+// script.js
+const modal = document.getElementById('modal');
+const fecharModalButton = document.getElementById('fecharModal');
+
+// Função para fechar a caixa modal
+function fecharModal() {
+    modal.style.display = 'none';
+}
+
+// Adiciona evento de clique ao botão de fechar a caixa modal
+fecharModalButton.addEventListener('click', fecharModal);
+
+// Adiciona evento de clique fora da caixa modal para fechar
+window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+        fecharModal();
+    }
+});
+
+// Adiciona evento de pressionar a tecla Esc para fechar a caixa modal
+window.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        fecharModal();
+    }
+});
